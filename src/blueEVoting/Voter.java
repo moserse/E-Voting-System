95e94c0c1730de59d8754db0ada8abfa6da52d87
Stefan Moser <moserse@users.noreply.github.com>
2016-10-30 14:25:54 -0400

    Update Voter.java

--- src/blueEVoting/Voter.java
+++ src/blueEVoting/Voter.java
@@ -1,5 +1,8 @@
 package blueEVoting;
 
+/*Voter class is unique to the user, storing their specific ID, date of birth, name and an assurance that the user
+	has or has not already casted a vote. */
+
 public class Voter {
 	
 	int voterID;
@@ -7,13 +10,16 @@ public class Voter {
 	String voterName;
 	boolean didCastVote;
 	
+	/*Sees if voter ID's storage has already been filled with votes to ensure that the votes cannot be doubled*/
 	boolean didCastVote( int voterID, Candidate candidate) { 
 		return true;
 	}
 	
+	/*compare hash'd ID within ballot*/
 	boolean compareHash(Ballot ballot) {
 		return false;
 	}
 	
+	/*Vote reversal in case of user accidental vote or mind change*/
 	void reverseVote (int voterID, Candidate candidate ) {};
 }
