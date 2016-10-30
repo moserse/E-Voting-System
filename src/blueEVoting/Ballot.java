7d0b02d86d19bd9a7434bcb8a4c65e3bc54332ac
Stefan Moser <moserse@users.noreply.github.com>
2016-10-30 14:06:46 -0400

    Update Ballot.java

--- src/blueEVoting/Ballot.java
+++ src/blueEVoting/Ballot.java
@@ -1,5 +1,9 @@
 package blueEVoting;
 
+/*short-term data storing class called ballot
+	gets candidate information and stores user data such as their vote and hashed ID number
+	the ballot is submitted or cancelled*/
+
 public class Ballot {
 	
 	String candidateName;
