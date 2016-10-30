868b939c0fe565b51712e9b0914fe4f5713d1a85
Stefan Moser <moserse@users.noreply.github.com>
2016-10-30 14:19:08 -0400

    Update Display.java

--- src/blueEVoting/Display.java
+++ src/blueEVoting/Display.java
@@ -1,5 +1,8 @@
 package blueEVoting;
 
+/*Display class acts that the interface to the user. Initially this will be tested with print statements
+	but as we begin coding we will implement Java Swing and use that to create our UI*/
+
 public class Display {
 	
 	int height, width, timer;
@@ -7,13 +10,29 @@ public class Display {
 	Candidate selectedCandidate;
 	
 	void displayVerification() {};
+	
+	/*exit the program*/
 	void exit() {};
+	
+	/*Change display view to the next option*/
 	void displayNextView() {};
+	
+	/*select candidate with checkbox/mouseClick interaction*/
 	void selectCandidate() {};
+	
+	/*click checkmark box to select or deselect*/
 	void setCheckmark() {};
+	
+	/*submission process dedicated on mouse input on button*/
 	void submitSelection() {};
+	
+	/*If the user is absent for 60 seconds, the software will initiate a timeout, erasing short-term voter data*/
 	void timeout() {};
+	
+	/*display admin panel and options following admin login*/
 	void displayAdminPanel() {};
+	
+	/*admin only method to reveal results*/
 	void showResults() {};
 
 }
