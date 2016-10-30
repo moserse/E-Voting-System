1534002ec14561b4059bd1e7ef3aee8bc6f52f39
Stefan Moser <moserse@users.noreply.github.com>
2016-10-30 14:23:10 -0400

    Update VoteController.java

--- src/blueEVoting/VoteController.java
+++ src/blueEVoting/VoteController.java
@@ -1,12 +1,21 @@
 package blueEVoting;
 
+/*Vote controller for ballot so that ballot can interact with database*/
+
 public class VoteController {
 	
 	Ballot localBallot;
 	
+	/*submission of ballot just prior to database storage*/
 	void submitBallot() {};
+	
+	/*user option to modify their ballot in case of fickleness or accidentaly wrong choice*/
 	void modifyBallot() {};
+	
+	/*a submission of initial selection from user, not entirely final*/
 	void submitSelection() {};
+	
+	/*final commit of user vote activity selection ready to be transfered to database*/
 	void commitSelections() {};
 
 }
