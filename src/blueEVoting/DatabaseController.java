c403d3979ae144e5304310eb63623fa0f321ebea
Stefan Moser <moserse@users.noreply.github.com>
2016-10-30 14:15:38 -0400

    Update DatabaseController.java

--- src/blueEVoting/DatabaseController.java
+++ src/blueEVoting/DatabaseController.java
@@ -1,21 +1,38 @@
 package blueEVoting;
 
+/*This class exists to allow interaction between vote control objects/classes and the database. 
+	Allows for reading and writing of file, hashing of IDs and vote data to allow for safe
+	transfer between user, vote control, and database*/
+
 public class DatabaseController {
 	
 	Ballot ballot;
 	Candidate candidate;
 	int candidateCount;
 	
+	/*read database file from database*/
 	void readDatabaseFile() {
 		
 	}
-	
+	/*write onto databasefile using TBD encryption protocol*/
 	void writeDatabaseFile() {}
+	
+	/*Admin only method that reveals results to a verified official. */
 	void showResults(Admin admin) {}
+	
+	/*Changes candidate within database*/
 	void changeCandidate(Candidate candidate) {}
+	
+	/*submission of ballot*/
 	void submitBallot() {};
+	
+	/*Validaton of Admin as an extra security measure for access to vote data and user registration numbers*/
 	void validateAdmin() {};
+	
+	/*hashing of Voter identificaiton via TBD protocol*/
 	void hashVoterID() {};
+	
+	/*Validation of voter similar to that of Admin process*/
 	void validateVoter() {};
 
 }
